b6e5db1bc03a5cf03f1d9b5616556d0760f478db
Paul Grigoras <paul.grigoras90@gmail.com>
2014-01-31 12:59:33 +0000

    Minor tweaks to solution for simple equations.

--- 2-complete-search/simple-eqs/solution/Main.java
+++ 2-complete-search/simple-eqs/solution/Main.java
@@ -2,10 +2,12 @@ import java.util.*;
 import java.io.*;
 
 import static java.lang.Math.*;
+import static java.lang.System.out;
 
 public class Main {
 
     static int a, b, c;
+
     public static void main (String [] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         sc.nextInt(); // skip number of tests
@@ -13,28 +15,27 @@ public class Main {
             a = sc.nextInt();
             b = sc.nextInt();
             c = sc.nextInt();
-            solve(30);
+            solve();
         }
     }
 
-    public static void solve(int bound) {
-        for (int x = -bound; x <= bound; x++)
-            for (int y = x + 1; y <= bound; y++)
-                if (check(x, y, (a - x - y)))
+    public static void solve() {
+        int bound = 34;
+        for (int x = -bound; x <= bound; x++) {
+            for (int y = x + 1; y <= bound; y++) {
+                int z = a - x - y;
+                if ( y != z && z != x &&
+                     x * y * z == b &&
+                     x * x + y * y + z * z == c) {
+                    int min = min(x, min(y, z));
+                    int max = max(x, max(y, z));
+                    int mid = a - min - max;
+                    out.printf("%d %d %d\n", min, mid, max);
                     return;
-        System.out.println("No solution.");
+                }
+            }
+        }
+        out.println("No solution.");
     }
 
-    public static boolean check(int x, int y, int z) {
-        if (x != z && y != z &&
-            z * y * x == b &&
-            z * z + y * y + x * x == c) {
-            int min = min(x, min(y, z));
-            int max = max(x, max(y, z));
-            int mid = a - min - max;
-            System.out.format("%d %d %d\n", min, mid, max);
-            return true;
-        }
-        return false;
-    }
 }
