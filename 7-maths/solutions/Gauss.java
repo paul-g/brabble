6e4391250489155afaa630852d121ef096ce5eb5
Paul Grigoras <paul.grigoras90@gmail.com>
2014-03-21 12:44:49 +0000

    Add Gauss.

--- 7-maths/solutions/Gauss.java
+++ 7-maths/solutions/Gauss.java
@@ -1,7 +1,86 @@
-class Java {
+import java.util.*;
+import java.io.*;
 
-    public static void main(String[] args) {
+class Gauss {
 
+    private static final double EPS = 0.00000001;
+
+    static void swapRows(double m[][], int i, int j) {
+        for (int k = 0; k < m[i].length; k++) {
+            double t = m[i][k];
+            m[i][k] = m[j][k];
+            m[j][k] = t;
+        }
+    }
+
+    static void printMat(double m[][]) {
+        for (int i = 0; i < m.length; i++)
+            System.out.println(Arrays.toString(m[i]));
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(new File("gauss.in"));
+
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        double mat[][] = new double[n][m + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m + 1; j++)
+                mat[i][j] = sc.nextDouble();
+        }
+
+        int i = 0, j = 0;
+        // j represents the column, i the row
+        while (i < n && j < m) {
+
+            // find a non-zero in the current column
+            int k;
+            for (k = i; k < n; k++)
+                if (Math.abs(mat[k][j]) > EPS)
+                    break;
+
+            if (k == n) {
+                j++;
+                continue;
+            }
+
+            // swap this row with the current i
+            swapRows(mat, i, k);
+
+            // divide row by pivot
+            double p = mat[i][j];
+            for (int kk = 0; kk < m + 1; kk++)
+                mat[i][kk] /= p;
+            mat[i][j] = 1;
+
+            // update all rows below the pivot row
+            for (int kk = i + 1; kk < n; kk++) {
+                // subtract the pivot row
+                double z = mat[kk][j];
+                for (int l = 0; l < m + 1; l++)
+                    mat[kk][l] -= z * mat[i][l];
+            }
+
+            ++i;
+            ++j;
+        }
+
+        double x[] = new double[m + 1];
+        // rebuild the solution
+        for (i = n - 1; i >= 0; i--) {
+            for (j = 0; j < m; j++) {
+                if (Math.abs(mat[i][j]) < EPS) continue;
+                x[j] = mat[i][m];
+                for (int k = j + 1; k < m; k++)
+                    x[j] -= x[k] * mat[i][k];
+                break;
+            }
+        }
+
+        System.out.println(Arrays.toString(x));
     }
 
 }
